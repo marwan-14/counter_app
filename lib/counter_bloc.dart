9559a84eeabef64b3ec5df055b76e2739c46fe69
marwan-14 <marwanelrasef14@gmail.com>
2024-08-15 12:51:13 +0200

    delete unwanted files

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,18 +1,1 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:untitled5/counter_event.dart';
 
-import 'counter_cubit.dart';
-
-class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterInitial(counterValue: 1, wasIncremented: true)) {
-    on<CounterEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
-
-}
-
-// The CounterInitial class should extend the CounterState class
-class CounterInitial extends CounterState {
-  CounterInitial({required super.counterValue, required super.wasIncremented});
-}
